35032cab8131ab15be0458c51428ed8c81be8e06
MahmoudWael <Mxheond9@gmail.com>
2018-10-23 00:26:33 +0200

    private meaasges

--- sockets/base.js
+++ sockets/base.js
@@ -1,11 +1,12 @@
 export default function (io) {
     var users = {};
-    
+    var socker_Ids = {}
     io.on('connection', (socket) => {
         console.log('a new user has conncted!');
         socket.on('send-username', function (username) {
             socket.username = username;
             users[username] = username;
+            socker_Ids[username] = socket;
             socket.broadcast.emit('user-connected', username);
             io.emit('update-usernames', users)
         });
@@ -13,6 +14,10 @@ export default function (io) {
         socket.on('chat message', function (msgData) {
             socket.broadcast.emit('chat message', msgData);
         });
+
+        socket.on('private-message', function(msgData, privateUserName){
+            socker_Ids[privateUserName].emit('private-message', msgData)
+        })
 
         socket.on('user-typing', (isTyping) => {
             socket.broadcast.emit('notify-typing', socket.username, isTyping)
